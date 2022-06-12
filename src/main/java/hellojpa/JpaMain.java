4231056422df7f0041e950f9034a13df42b4025e
ByeoungJoon Jeon <juni8453@naver.com>
2022-06-12 14:35:11 +0900

    영속성 컨텍스트 1,2 복습 완료

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,10 +14,16 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 2L);
-            em.remove(findMember);
+            Member memberA = new Member(150L, "A");
+            Member findMember150 = em.find(Member.class, 150L);
+            findMember150.setName("changeName150");
 
+            // 영속 컨텍스트가 flush() 되는 시점에 스냅샷과 엔티티의 현재 상태를 비교한다.
+            // 비교해서 달라진 Entity 를 찾고, 달라진게 있다면 자동으로 UPDATE 쿼리가 생성된다.
+            // 생성된 UPDATE 쿼리는 영속 컨텍스트의 쓰기 지연 SQL 저장소에 담긴다.
+            // 이제 트랜잭션을 commit() 하게되면 담겨있던 UPDATE 가 실행되며 (flush) 엔티티 수정 값이 DB에 반영된다.
             tx.commit();
+
         } catch (Exception e) {
             tx.rollback();
         } finally {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,6 +1,8 @@
 package hellojpa;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -8,12 +10,16 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id
     private Long id;
 
-    // 만약 DB 의 컬럼명이 username 이라면, 이렇게 컬럼명을 지정할 수도 있음.
-    // @Column(name = "username")
     private String name;
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
