7a43bc18d6f8d6417bd63da0c529989753306c00
ByeoungJoon Jeon <juni8453@naver.com>
2022-06-15 19:45:20 +0900

    기본키 매핑까지 복습 완료

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,16 +14,16 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member memberA = new Member(150L, "A");
-            Member findMember150 = em.find(Member.class, 150L);
-            findMember150.setName("changeName150");
+            Member member = new Member();
+            member.setUsername("C");
 
-            // 영속 컨텍스트가 flush() 되는 시점에 스냅샷과 엔티티의 현재 상태를 비교한다.
-            // 비교해서 달라진 Entity 를 찾고, 달라진게 있다면 자동으로 UPDATE 쿼리가 생성된다.
-            // 생성된 UPDATE 쿼리는 영속 컨텍스트의 쓰기 지연 SQL 저장소에 담긴다.
-            // 이제 트랜잭션을 commit() 하게되면 담겨있던 UPDATE 가 실행되며 (flush) 엔티티 수정 값이 DB에 반영된다.
+            System.out.println("em.persist 이전");
+            em.persist(member);
+            System.out.println("em.persist 이후");
+
+            System.out.println("트랙잭션 커밋 이전");
             tx.commit();
-
+            System.out.println("트랙잭션 커밋 이후");
         } catch (Exception e) {
             tx.rollback();
         } finally {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -7,19 +7,19 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
-@Entity
 @Getter
 @Setter
+@Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @Column(name = "name")
+    private String username;
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 }
+
+
